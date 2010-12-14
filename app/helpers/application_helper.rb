707c67d717b4300a137da1840d727c13a5ed7677
Cyril <cyril@ubuntu.ubuntu-domain>
2010-12-14 08:49:17 +0100

    Ajout de fonction helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,44 @@ module ApplicationHelper
       return false
     end
   end
+
+  def set_date_format(date,format)
+    return date.strftime(format)
+  end
+
+  def get_time(date)
+    return date.strftime("%H\:%M")
+  end
+
+  def devise_errors!
+    format_errors(resource)
+  end
+
+  def form_errors!(form)
+    format_errors(form)
+  end
+
+  def format_errors(object)
+    return "" if object.errors.empty?
+
+    messages = object.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
+    sentence = "#{pluralize(object.errors.count, "erreur")}"
+
+    html = <<-HTML
+<div id="error_explanation">
+<h3>#{sentence}</h3>
+<ul>#{messages}</ul>
+</div>
+    HTML
+
+    html.html_safe
+  end
+
+  def format_keywords(keywords_string)
+    keywords = []
+    keywords_string.split(',').each do |keyword|
+      keywords.push(I18n.t("keywords.#{keyword}"))
+    end
+    return keywords * ', '
+  end
 end
