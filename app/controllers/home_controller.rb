c388fc957edb48009afebce2d493d2b3f39f8ec1
Cyril <cyril@ubuntu.ubuntu-domain>
2010-12-14 08:50:04 +0100

    Ajout des objets pour l'action index du controller home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
   def index
+    @medias = Media.find_all_by_media_container_type('Gallery').reverse[0..9]
+    @informations = Information.all.reverse[0..6]
   end
 
 end
