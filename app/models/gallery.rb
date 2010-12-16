597e9c7022caf0a2e43e054f7fe8f44b5194d22c
Cyril <cyril@ubuntu.ubuntu-domain>
2010-12-16 08:39:27 +0100

    Ajout de la destruction des enfants pour les galeries

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,6 +1,6 @@
 class Gallery < ActiveRecord::Base
   belongs_to :user
-  has_many :drawings, :as => :media_container, :class_name => 'Media'
+  has_many :drawings, :as => :media_container, :class_name => 'Media', :dependent => :destroy
 
   validates :name, :presence => true
   validates :keywords, :presence => true
