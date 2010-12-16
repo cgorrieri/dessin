127e4454189d9b1aca04cac22639b1863a978f2a
Cyril <cyril@ubuntu.ubuntu-domain>
2010-12-16 08:38:42 +0100

    Changement de la redirection lors de la connection et lors de update des user vers le edit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
   include ApplicationHelper
   protect_from_forgery
+
+  def after_update_path_for(resource)
+    private_datas_user_path
+  end
+
+  def after_sign_in_path_for(resource)
+    private_datas_user_path
+  end
 end
