efc961142a98c224225e7a12e26b7bb314b24ed0
Cyril <cyril@ubuntu.ubuntu-domain>
2010-12-16 17:46:46 +0100

    Ajout de la suppression en cascade

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
 
-  has_many :galleries
+  has_many :galleries, :dependent => :destroy
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable, :lockable and :timeoutable
   devise :database_authenticatable, :registerable,
