c71f4b095f1d4dd6d35bc570a2430a735fa4706a
Cyril <cyril@ubuntu.ubuntu-domain>
2011-01-12 15:32:20 +0100

    Ajout de la methode pour la validation des mots clès + rectification du problème sur les mots cles quand erreur

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -2,8 +2,22 @@ class Gallery < ActiveRecord::Base
   belongs_to :user
   has_many :drawings, :as => :media_container, :class_name => 'Media', :dependent => :destroy
 
+  MIN_KEYWORDS = 1
+  MAX_KEYWORDS = 4
+
   validates :name, :presence => true
-  validates :keywords, :presence => true
+  validate :keywords_validation
+
+  def keywords_validation
+    if self.keywords.nil?
+      number_of_keywords = 0
+    else
+      number_of_keywords = self.keywords.split(',').count
+    end
+    if number_of_keywords < MIN_KEYWORDS || number_of_keywords > MAX_KEYWORDS
+      errors.add(:keywords , :keywords_must_be_contains_between, :min => MIN_KEYWORDS, :max => MAX_KEYWORDS)
+    end
+  end
 
   @@per_page = 10
   
